926f7917e8ee87ef2d3df77ce3db2c090f08d7fb
JulijusErgul <julijus.ergul@gmail.com>
2020-08-09 20:31:19 +0200

    Add project files.

---
+++ REST-API/Models/TaskList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API.Models
+{
+    public class TaskList
+    {
+        public int TaskListId { get; set; }
+        public string TaskListName { get; set; }
+        public string TaskListDescription { get; set; }
+        public int TaskCount { get; set; }
+    }
+}
---
+++ REST-API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserImage { get; set; }
+    }
+}
